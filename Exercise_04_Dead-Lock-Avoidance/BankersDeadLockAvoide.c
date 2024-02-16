c9f6b2e0a288a3011d41f09ff4c39ee4fe5b5539
Gautam Ankoji <120157430+gautamankoji@users.noreply.github.com>
2024-02-16 23:41:34 +0530

    Update BankersDeadLockAvoide.c

--- Exercise_04_Dead-Lock-Avoidance/BankersDeadLockAvoide.c
+++ Exercise_04_Dead-Lock-Avoidance/BankersDeadLockAvoide.c
@@ -103,52 +103,30 @@ Enter the no of resources : 3
 
 
 Enter the Max Matrix for each process :
-For process 1 : 7
-5
-3
+For process 1 : 7 5 3
 
-For process 2 : 3
-2
-2
+For process 2 : 3 2 2
 
-For process 3 : 7
-0
-2
+For process 3 : 7 0 2
 
-For process 4 : 2
-2
-2
+For process 4 : 2 2 2
 
-For process 5 : 4
-3
-3
+For process 5 : 4 3 3
 
 
 Enter the allocation for each process :
-For process 1 : 0
-1
-0
+For process 1 : 0 1 0
 
-For process 2 : 2
-0
-0
+For process 2 : 2 0 0
 
-For process 3 : 3
-0
-2
+For process 3 : 3 0 2
 
-For process 4 : 2
-1
-1
+For process 4 : 2 1 1
 
-For process 5 : 0
-0
-2
+For process 5 : 0 0 2
 
 
-Enter the Available Resources : 3
-3
-2
+Enter the Available Resources : 3 3 2
 
 Max matrix:     Allocation matrix:
 7 5 3           0 1 0
